0876f82e9219378817002b793ce33026b9279de7
VU Anh Tuan <vuanhtuan1012@gmail.com>
2022-04-05 08:17:52 +0200

    unit tests for Fibonacci problem

---
+++ tests/fib.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: anh-tuan.vu
+# @Date:   2022-04-04 22:03:48
+# @Email:  anh-tuan.vu@outlook.com
+# @Last Modified by:   anh-tuan.vu
+# @Last Modified time: 2022-04-05 06:50:25
+
+import unittest
+from fib_memoization import fib
+
+
+class Fibonacci(unittest.TestCase):
+    def test_fib6(self):
+        self.assertEqual(fib(6), 8)
+
+    def test_fib7(self):
+        self.assertEqual(fib(7), 13)
+
+    def test_fib8(self):
+        self.assertEqual(fib(8), 21)
+
+    def test_fib50(self):
+        self.assertEqual(fib(50), 12586269025)
+
+
+if __name__ == '__main__':
+    unittest.main()
