c8ab09d92635808b23bc8ec2d06868a212ef4df0
VU Anh Tuan <vuanhtuan1012@gmail.com>
2022-04-06 03:58:27 +0200

    add __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: anh-tuan.vu
+# @Date:   2022-04-06 03:42:38
+# @Email:  anh-tuan.vu@outlook.com
+# @Last Modified by:   anh-tuan.vu
+# @Last Modified time: 2022-04-06 03:42:38
