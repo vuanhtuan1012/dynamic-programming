db7eecbf2d50c10029ad2f3e8447cb0fea8f4a40
VU Anh Tuan <vuanhtuan1012@gmail.com>
2022-04-09 23:45:05 +0200

    small modification

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -3,7 +3,7 @@
 # @Date:   2022-04-04 22:03:48
 # @Email:  anh-tuan.vu@outlook.com
 # @Last Modified by:   anh-tuan.vu
-# @Last Modified time: 2022-04-06 03:37:10
+# @Last Modified time: 2022-04-09 23:37:36
 
 import unittest
 from fib_memorization import fib
@@ -11,16 +11,16 @@ from fib_memorization import fib
 
 class Fibonacci(unittest.TestCase):
     def test_fib6(self):
-        self.assertEqual(fib(6), 8)
+        self.assertEqual(fib(6, dict()), 8)
 
     def test_fib7(self):
-        self.assertEqual(fib(7), 13)
+        self.assertEqual(fib(7, dict()), 13)
 
     def test_fib8(self):
-        self.assertEqual(fib(8), 21)
+        self.assertEqual(fib(8, dict()), 21)
 
     def test_fib50(self):
-        self.assertEqual(fib(50), 12586269025)
+        self.assertEqual(fib(50, dict()), 12586269025)
 
 
 if __name__ == '__main__':
--- tests/test_grid_traveler.py
+++ tests/test_grid_traveler.py
@@ -3,7 +3,7 @@
 # @Date:   2022-04-09 10:16:28
 # @Email:  anh-tuan.vu@outlook.com
 # @Last Modified by:   anh-tuan.vu
-# @Last Modified time: 2022-04-09 10:19:48
+# @Last Modified time: 2022-04-09 23:37:06
 
 import unittest
 from grid_traveler import gridTraveler
@@ -11,16 +11,16 @@ from grid_traveler import gridTraveler
 
 class GridTraveler(unittest.TestCase):
     def test_gridTraveler11(self):
-        self.assertEqual(gridTraveler(1, 1), 1)
+        self.assertEqual(gridTraveler(1, 1, dict()), 1)
 
     def test_gridTraveler23(self):
-        self.assertEqual(gridTraveler(2, 3), 3)
+        self.assertEqual(gridTraveler(2, 3, dict()), 3)
 
     def test_gridTraveler32(self):
-        self.assertEqual(gridTraveler(3, 2), 3)
+        self.assertEqual(gridTraveler(3, 2, dict()), 3)
 
     def test_gridTraveler33(self):
-        self.assertEqual(gridTraveler(3, 3), 6)
+        self.assertEqual(gridTraveler(3, 3, dict()), 6)
 
     def test_gridTraveler1818(self):
         self.assertEqual(gridTraveler(18, 18), 2333606220)
